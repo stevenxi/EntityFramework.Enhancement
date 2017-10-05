56ff9063cb7d16c7f8e01bf583f7da53c907ac69
Steven <Steven4ever@gmail.com>
2017-10-05 18:25:48 +0100

    Base

---
+++ Source/EntityFramework.Enhancement.Core/Context/ExtendedDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework.Enhancement.Core.Context
+{
+    public class ExtendedDbContext : DbContext
+    {
+        public ExtendedDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        protected ExtendedDbContext() 
+        {
+        }
+
+
+
+    }
+}
